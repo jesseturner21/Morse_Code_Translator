a68bd2a47b9323f528b50c11ae14a5aa6db856e5
jesseturner21 <jesseturner2021@gmail.com>
2023-07-28 07:09:56 -0700

    Cleaner user experience

--- main.py
+++ main.py
@@ -4,38 +4,39 @@ import pandas as pd
 df = pd.read_csv("morse_code.csv")
 morse = df.to_dict()
 
-
-# Ask user if they want to English(1) or Morse Code(2)
-translate_to = input('\nEnglish -> Morse (E)\nMorse -> English (M)\nQuit (Q)\n').upper()
-go_on = translate_to
+# Ask user what way to translate
+translate_from = input('\nEnglish -> Morse   (E)\nMorse   -> English (M)\n           Quit    (Q)\n').upper()
+go_on = translate_from
+# Gets initial phrase
 phrase = input("\nEnter phrase to translate:\n").upper()
 
+# Continues till user types Q
 while go_on != 'Q':
 
-    if translate_to == 'M':
+    # --------MORSE TO ENGLISH-------- #
+    if translate_from == 'M':
         new_phrase = ''
-        # split the input bc morse has a sace between letters
+        # split the input bc morse has a space between letters
         phrase = phrase.split(' ')
         for letter in phrase:
-
             if letter == '':
                 new_phrase += ' '
             else:
                 translate = df[df['morse_code'] == letter]['english'].values[0]
                 new_phrase += translate
-    elif translate_to == 'E':
+    # --------ENGLISH TO MORSE-------- #
+    elif translate_from == 'E':
         new_phrase = ''
         for letter in phrase:
             if letter == ' ':
                 new_phrase += ' '  # add two spaces between morse-words
             else:
                 translate = df[df['english'] == letter]['morse_code'].values[0]
-                new_phrase += translate + ' '  # add space between morse-letters
+                new_phrase += translate + ' '  # add one space between morse-letters
 
     print(new_phrase)
-    # Get the next one and see if it should go on
+    # Get the next phrase and see if it should continue
     phrase = input().upper()
     go_on = phrase
-
 
 print('Exiting')
